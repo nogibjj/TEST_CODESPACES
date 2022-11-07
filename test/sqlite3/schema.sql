217aef495f3da095535578d46aabff2d86468284
Echo chen <90811429+echodpp@users.noreply.github.com>
2022-11-07 04:33:33 +0000

    project 3

--- test/sqlite3/schema.sql
+++ test/sqlite3/schema.sql
@@ -1,1 +1,26 @@
-drop table if exists "users";
+CREATE database test;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+-- Path: test/sqlite3/data.sql
+
+INSERT INTO user (username, password) VALUES ('alice', 'alice');
+INSERT INTO user (username, password) VALUES ('bob', 'bob');
+INSERT into post (author_id, title, body) VALUES (1, 'Hello', 'Hello world!');
+INSERT into post (author_id, title, body) VALUES (1, 'Goodbye', 'Goodbye world!');
--- test/sqlite3/sqlite3.py
+++ test/sqlite3/sqlite3.py
@@ -1,44 +1,45 @@
-# import sqlite3
+import sqlite3
 
-# import click
-# from flask import current_app, g
-# from flask.cli import with_appcontext
-
-
-# def get_db():
-#     if 'db' not in g:
-#         g.db = sqlite3.connect(
-#             current_app.config['DATABASE'],
-#             detect_types=sqlite3.PARSE_DECLTYPES
-#         )
-#         g.db.row_factory = sqlite3.Row
-
-#     return g.db
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 
 
-# def close_db(e=None):
-#     db = g.pop('db', None)
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
 
-#     if db is not None:
-#         db.close()
-
-
-# def init_db():
-#     db = get_db()
-
-#     with current_app.open_resource('schema.sql') as f:
-#         db.executescript(f.read().decode('utf8'))
+    return g.db
 
 
-# @click.command('init-db')
-# @with_appcontext
-# def init_db_command():
-#     """Clear the existing data and create new tables."""
-#     init_db()
-#     click.echo('Initialized the database.')
+def close_db(e=None):
+    db = g.pop('db', None)
 
-# def init_app(app):
-#     app.teardown_appcontext(close_db)
-#     app.cli.add_command(init_db_command)
+    if db is not None:
+        db.close()
 
-# # Path: test/__init__.py
+#build the database in schema.sql
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+# Path: test/__init__.py
